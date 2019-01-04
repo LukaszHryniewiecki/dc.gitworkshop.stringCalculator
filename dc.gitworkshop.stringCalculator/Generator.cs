6abe593997194ca8fd344b3af52231bdd2883301
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-01-04 15:06:40 +0100

    use random numbers in generator

--- dc.gitworkshop.stringCalculator/Generator.cs
+++ dc.gitworkshop.stringCalculator/Generator.cs
@@ -1,13 +1,16 @@
-﻿namespace dc.gitworkshop.stringCalculator
+﻿using System;
+
+namespace dc.gitworkshop.stringCalculator
 {
     public class Generator
     {
         public static string Random(int count, string delimiter, bool allowNegatives = false)
         {
+            Random r = new Random();
             string result = "";
             for (int i = 0; i < count; i++)
             {
-                result += "0" + delimiter;
+                result += r.Next() + delimiter;
             }
             if(result.Length!=0)
                 return result.Substring(0, result.Length - 1);
